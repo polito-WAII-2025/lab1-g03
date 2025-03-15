5d27c81cffd7624eef5d8868baafc5d30300535e
criicky <scan.riccardo@gmail.com>
2025-03-15 14:44:04 +0100

    new: data class Waypoint

--- RouteAnalyzer/src/main/kotlin/Main.kt
+++ RouteAnalyzer/src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 package org.example
 
+data class Waypoint( val timestamp: String, val latitude: Double, val longitude: Double )
+
 fun main( ) {
     println("Hello World!")
   println("This is a test")
